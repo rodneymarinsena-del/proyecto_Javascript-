c327f7789f395928ebdc601144d2b70d57c38b24
pcSena <pcSena@gamil.com>
2026-07-31 10:24:27 -0500

    tercer commit

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const express = require('express');
+//const express = require('express');
+import express from 'express';
+//leer el archivo .env 
+import {configDotenv} from "dotenv"
+configDotenv()
+
 const app = express();
 const port = process.env.puerto || 7000;
-app.get("/", (_, res) => {res.send("Aprendiendo express,ficha 3407181");
+app.get("/", (_, res) => {res.send("Aprendiendo express,ficha 3407181,adso en el sena de la 52 31/07/26");
 });
 app.listen(port, () => {
 console.log( `Servidor en funcionamiento en el puerto: ${port}`);
---
+++ readme.txt
@@ -0,0 +1,7 @@
+comandos para iniciar 
+npm install -g yarn
+mkdir intro-express-js-pepito-perez
+cd intro-express-js-pepito-perez
+npm install express 
+node app.js 
+http://localhost: 
