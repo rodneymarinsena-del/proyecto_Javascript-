43e5fc4a9ab9391823a3857bf8c8aebf5b1b92df
Your Name <sena@gmail.com>
2026-08-21 11:28:24 -0500

    21/08/2026

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
+
 const express = require('express');
 const app = express();
 const port = 3000;
-app.get("/", (_, res) => {res.send("API REST - APRENDICES ");
+//configurar para la lectura del archivo
+const sistemaArchivo = require("fs")
+const ruta = require("path")
+const rutaArchivoJson = ruta.join(__dirname, "datos.json")
+
+app.get("/", (_, res) => {
+res.send("API Rest - Aprendices");
 });
 app.listen(port, () => {
 console.log( `Servidor en funcionamiento en el puerto: ${port}`);
+});
+
+// OTRO ENDPOINT
+app.get("/otra", (_, res) => {
+res.send("API Rest Aprendices");
+});
+
+//endpoint para ver los datos del archivo
+app.get("/otra/aprendices", (req, res)=>{
+    //datos vienen del archivo
+    sistemaArchivo.readFile(rutaArchivoJson, "utf-8", (error, datos)=>{
+        if(error){
+            return res.json({Error: "No se puede leer los datos."})
+        }
+        const listaAprendices = JSON.parse(datos)
+        res.json(listaAprendices)
+    })
 });
