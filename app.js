c3c9c5ec9ba2bc99026be8f1a9aae5838a8fdb41
Your Name <sena@gmail.com>
2026-08-21 10:21:33 -0500

    segundo commit inicio proyecto

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-app.get("/", (_, res) => {res.send("Aprendiendo express,ficha 3407181");
+app.get("/", (_, res) => {res.send("API REST - APRENDICES ");
 });
 app.listen(port, () => {
 console.log( `Servidor en funcionamiento en el puerto: ${port}`);
